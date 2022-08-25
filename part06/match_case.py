57fd3b55bdd9860a1d539348c9bcb34eae84b38d
프로그래머스 교육팀 <soorte214@gmail.com>
2022-08-25 23:56:32 +0900

    Create match_case.py

---
+++ part06/match_case.py
@@ -0,0 +1,12 @@
+status = 303
+match status:
+    case 200:
+        print("success")
+    case 300:
+        print("redirect")
+    case 400:
+        print("client error")
+    case 500:
+        print("server error")
+    case _:
+        print("not match")
